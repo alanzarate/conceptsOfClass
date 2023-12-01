43644e7defc26d980f4152336dc1c4c8ed38ab7d
alanzarate <alnzarate@gmail.com>
2023-12-01 07:06:07 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as conceptsData from '../assets/ownData.json';
 
+interface DataOwn{
+  title: string;
+  description: string;
+  description2: string;
+  image: string;
+  keywords: string[];
+  example: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] 
 })
-export class AppComponent {
-  title = 'conceptsOfClass';
+export class AppComponent implements OnInit{
+  data: any = (conceptsData as any).default;
+  indexSel : number = 1;
+  selected: DataOwn = {
+    title: "",
+    description: "",
+    description2: "",
+    image: "",
+    keywords: [],
+    example: ""
+  }
+  constructor() {
+    
+  }
+  ngOnInit(){
+     this.selected = this.data[this.indexSel]
+  }
+  makeThisCurrnt(ind:number){
+    this.indexSel = ind;
+    this.selected = this.data[ind]
+  }
 }
